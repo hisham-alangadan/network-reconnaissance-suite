4a4450ce18dba9c66d16b3ece64c40ad43217d69
Hisham Alangadan <hishamal2002@gmail.com>
2024-04-21 09:16:49 +0530

    Added till the output page

--- scripts/ip.js
+++ scripts/ip.js
@@ -1,17 +1,77 @@
+try{
+    const ipInput = document.getElementById('ip-input');
 
-const ipInput = document.getElementById('ip-input');
-
-ipInput.addEventListener("keypress", function(event)
-{
-    if(event.key === "Enter")
+    ipInput.addEventListener("keypress", function(event)
     {
-        event.preventDefault();
-        console.log(ipInput.value);
+        if(event.key === "Enter")
+        {
+            event.preventDefault();
+            console.log(ipInput.value);
 
-        if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipInput.value)) {  
-            window.location.href = ("./port.html");
-        }  
-        alert("You have entered an invalid IP address!")  
+            if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipInput.value)) {  
+                window.location.href = ("./port.html");
+            }  
+            alert("You have entered an invalid IP address!")  
+        }
+    });
+} catch(e) {}
+
+try{
+    const startPort = document.getElementById('start-port-input');
+    const endPort = document.getElementById('end-port-input');
+
+    startPort.addEventListener("keypress", function(event)
+    {
+        if(event.key === "Enter")
+        {
+            acceptPort();
+        }
+    })
+
+    endPort.addEventListener("keypress", function(event)
+    {
+        if(event.key === "Enter")
+        {
+            acceptPort();
+        }
+    })
+
+    function acceptPort()
+    {
+        const start = parseInt(startPort.value);
+        const end = parseInt(endPort.value);
+
+        console.log("checking...")
+        if(isNaN(start) || isNaN(end)){
+            console.log("not a number")
+        }
+        else
+        {
+            if(start <= 0 || start >= 65500 || end <=  0 || end >= 65500)
+            {
+                alert("please enter a port bw 0 and 65500");
+                console.log(start)
+                console.log(end)
+            }
+            else
+            {
+                console.log("Port accepted");
+                window.location.href = ("./protocol.html")
+            }
+
+        }
     }
-});
+} catch(e) {}
 
+var protocol = "DEFAULT";
+
+try{
+    function handleTcp() {
+        window.location.href = ("./flags.html")
+    }
+
+    function handleUdp() {
+        window.location.href = ("./output.html")
+    }
+
+} catch(e) {}
--- todo.md
+++ todo.md
@@ -1,6 +1,7 @@
 - port multiple and single input
 - beautify error
 - [text](https://www.youtube.com/watch?v=eVwH3VL1EsA)
+- titles for pages
 
 - anything following the mouse
 - animations like [fds](https://www.mentimeter.com/features/quiz-presentations)
